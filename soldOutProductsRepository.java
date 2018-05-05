857a30f53898eae3fc71d82bf54b8d7ae2729742
MinaAdelZekry <minaa996@gmail.com>
2018-05-05 20:48:19 +0200

    Add files via upload

--- soldOutProductsRepository.java
+++ soldOutProductsRepository.java
@@ -1,10 +1,15 @@
 package com.example.demo.repositories;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
+import com.example.demo.entities.Offer;
 import com.example.demo.entities.SoldOutProduct;
 
 public interface soldOutProductsRepository extends CrudRepository <SoldOutProduct,Integer>{
+	public List<SoldOutProduct> findAllByStoreid(int storeid);
 	
 	
 }
